3ccd6c29ee3959ff46334abf998a89df3fb4481b
piangpor <pariwat302@gmail.com>
2017-04-26 12:07:15 +0700

    signup Regin

--- app/src/main/java/cph/kaewpila/pariwat/cpheasy/MainActivity.java
+++ app/src/main/java/cph/kaewpila/pariwat/cpheasy/MainActivity.java
@@ -1,12 +1,14 @@
 package cph.kaewpila.pariwat.cpheasy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private EditText userEditText, passwordEditText;
@@ -21,13 +23,39 @@ public class MainActivity extends AppCompatActivity {
 
         //Initial View
         initialview();
+
+        //contorller
+        contoller();
+
     }   //Main Method
 
+    private void contoller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+    }
+
     private void initialview() {
+
+
         userEditText = (EditText) findViewById(R.id.edtUser);
         passwordEditText = (EditText) findViewById(R.id.edtPassword);
         textView = (TextView) findViewById(R.id.txtNewRegis);
         button = (Button) findViewById(R.id.btnLogin);
     }
 
+    @Override
+    public void onClick(View v) {
+
+        //for textview
+        if (v == textView) {
+            //ย้ายการทำงานจากเมนไปหน้าsignup
+            Intent intent = new Intent(MainActivity.this,SigUpActivity.class);
+            startActivity(intent);
+        }
+
+        //fof Buttom
+        if (v == button) {
+
+        }
+    }
 }   //Main Class นี่คือคลาสหลัก
