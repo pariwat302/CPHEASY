dff2f18926b6d582b83705cfcb494e475a2785ff
piangpor <pariwat302@gmail.com>
2017-04-26 10:47:08 +0700

    initial view ok

--- app/src/main/java/cph/kaewpila/pariwat/cpheasy/MainActivity.java
+++ app/src/main/java/cph/kaewpila/pariwat/cpheasy/MainActivity.java
@@ -2,12 +2,32 @@ package cph.kaewpila.pariwat.cpheasy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Initial View
+        initialview();
+    }   //Main Method
+
+    private void initialview() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegis);
+        button = (Button) findViewById(R.id.btnLogin);
     }
-}
+
+}   //Main Class นี่คือคลาสหลัก
